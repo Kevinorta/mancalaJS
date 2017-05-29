406e98fd5a9bc50d65986ca7c78703f37ba26290
Kevin Orta <kevino6532@gmail.com>
2017-05-28 23:37:18 -0400

    ting

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-var ws = require("nodejs-websocket");
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+    .use((req, res) => res.sendFile(INDEX))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({
+    server
+});
 
 
-console.log("HELLO");
 global.player = 1;
 global.clients = [];
-// Scream server example: "hi" -> "HI!!!" 
-var server = ws.createServer(function (conn) {
 
-
-    clients[global.player] = conn;
-    conn.sendText("Hello player " + player + ". What is your name?")
-    clients[player] = conn;
+console.log("HELLO");
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    clients[global.player] = ws;
     if (player == 1) {
         player++;
     } else {
@@ -18,17 +30,61 @@ var server = ws.createServer(function (conn) {
     }
 
 
-
-    conn.on("text", function (str) {
+    ws.on('message', function incoming(str) {
+        console.log('received: %s', message);
         if (str.includes("msg ")) {
-            server.connections.forEach(function (conn) {
-                conn.sendText(str.substring('msg '.length))
-            })
+
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(str.substring('msg '.length));
+                }
+            });
 
         }
-    })
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    })
+    });
 
-}).listen(process.env.PORT || 5000)
+
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+
+
+
+
+
+
+
+//var ws = require("nodejs-websocket");
+//
+//
+//
+//global.player = 1;
+//global.clients = [];
+//// Scream server example: "hi" -> "HI!!!" 
+//var server = ws.createServer(function (conn) {
+//
+//
+//    clients[global.player] = conn;
+//    conn.sendText("Hello player " + player + ". What is your name?")
+//    clients[player] = conn;
+//    if (player == 1) {
+//        player++;
+//    } else {
+//        clients[1].sendText("Player two has joined you")
+//    }
+//
+//
+//
+//    conn.on("text", function (str) {
+//        if (str.includes("msg ")) {
+//            server.connections.forEach(function (conn) {
+//                conn.sendText(str.substring('msg '.length))
+//            })
+//
+//        }
+//    })
+//    conn.on("close", function (code, reason) {
+//        console.log("Connection closed")
+//    })
+//
+//}).listen(process.env.PORT || 5000)
